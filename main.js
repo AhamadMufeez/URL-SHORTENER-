bce73f89e22f936079c9e1d560cc38bebc9669cd
AhamadMufeez <mufeezahmad938@gmail.com>
2026-03-22 15:49:37 +0530

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+const inputBtn = document.getElementById('take-input');
+const copyBtn = document.getElementById('copy');
+const inputLink = document.getElementById('link');
+const outputDisplay = document.getElementById('show-link');
+
+inputBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  const link = inputLink.value;
+})
